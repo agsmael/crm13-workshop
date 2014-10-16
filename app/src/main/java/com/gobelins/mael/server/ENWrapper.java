d4ecb9527f3bb2cf08d493169a3467808026960b
mael <mael@avant-gout.com>
2014-10-16 09:59:40 +0200

    update libs

---
+++ app/src/main/java/com/gobelins/mael/server/ENWrapper.java
@@ -0,0 +1,38 @@
+package com.gobelins.mael.server;
+
+import android.content.Context;
+
+import com.echonest.api.v4.BasicPlaylistParams;
+import com.echonest.api.v4.EchoNestAPI;
+import com.echonest.api.v4.EchoNestException;
+import com.echonest.api.v4.Playlist;
+import com.gobelins.mael.crm13_workshop.R;
+
+public class ENWrapper {
+    private static ENWrapper sInstance;
+    private EchoNestAPI mApi;
+
+    public ENWrapper(String key) {
+        mApi = new EchoNestAPI(key);
+    }
+
+    public static ENWrapper with(Context context) {
+        if (sInstance == null) {
+            sInstance = new ENWrapper(context.getString(R.string
+                    .echo_nest_api));
+        }
+        return sInstance;
+    }
+
+    public Playlist getArtistRadio(int results, String artist) throws
+            EchoNestException {
+        BasicPlaylistParams params = new BasicPlaylistParams();
+        params.addIDSpace("7digital-US");
+        params.includeTracks();
+        params.addArtist(artist);
+        params.setType(BasicPlaylistParams.PlaylistType.ARTIST_RADIO);
+        params.setResults(results);
+        return mApi.createBasicPlaylist(params);
+    }
+}
+
